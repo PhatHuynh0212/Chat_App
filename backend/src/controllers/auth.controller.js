6f3a6471d0657231224e401faab0518422b2c326
huynhthanhphat <thanhphat02122003@gmail.com>
2024-12-16 17:11:22 +0700

    setup backend

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  res.send("Sign up");
+};
+
+export const login = (req, res) => {
+  res.send("Log in");
+};
+
+export const logout = (req, res) => {
+  res.send("Log out");
+};
