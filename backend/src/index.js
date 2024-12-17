2723fddfb74030fd64b7379f937f44c79af242ad
huynhthanhphat <thanhphat02122003@gmail.com>
2024-12-17 18:07:14 +0700

    code api message

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./config/database.js";
 import { ENV_VAR } from "./config/envVars.js";
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
