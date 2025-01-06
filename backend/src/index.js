efbea1f3811ea9d09ce663bf38c45287861dce83
huynhthanhphat <thanhphat02122003@gmail.com>
2025-01-06 17:54:42 +0700

    setup for deployment project

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,10 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./config/database.js";
 import { ENV_VAR } from "./config/envVars.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 const PORT = ENV_VAR.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
@@ -21,6 +23,13 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+
+if (ENV_VAR.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port:", PORT);
